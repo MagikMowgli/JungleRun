c1963fe91a00687ad3354fe0ad3ba10068af9322
MagikMowgli <86474206+MagikMowgli@users.noreply.github.com>
2024-11-09 17:41:03 +0000

    changed app to do hello world

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'You either run with the wolves or lead them, young mowgs!'
+    return 'Hello World!'
 
 
 if __name__== '__main__':
